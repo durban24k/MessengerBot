0d6d499d7cc650a171ec40f089c5e02ce2391fdb
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-12-17 22:32:43 +0300

    v1.0.0

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+let express=require('express');
+let bodyParser=require('body-parser');
+let path=require('path');
+let logger=require('morgan');
+
+let app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(logger('combined'));
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+     res.render('index.ejs');
+});
+
+let port = process.env.PORT || 3000
+app.listen(port,()=>{
+     console.log(`Server listening on port ${port}`);
+});
