c6108441ad4bcc9945ac7d78d262441000e59c2c
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-12-17 23:04:50 +0300

    public v1.0.2

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(logger('combined'));
 
+app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
